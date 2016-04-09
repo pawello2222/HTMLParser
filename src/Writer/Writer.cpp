01c8b7466b7bffba27fb2d06c510500f9bba109b
pawello2222 <pawello2222@gmail.com>
2016-04-09 14:45:26 +0200

    JSON writer initial commit

---
+++ src/Writer/Writer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Pawel Wiszenko on 09.04.2016.
+//
+
+#include "Writer.h"
---
+++ src/Writer/Writer.h
@@ -0,0 +1,15 @@
+//
+// Created by Pawel Wiszenko on 09.04.2016.
+//
+
+#ifndef HTMLPARSER_WRITER_H
+#define HTMLPARSER_WRITER_H
+
+
+class Writer
+{
+
+};
+
+
+#endif //HTMLPARSER_WRITER_H
